2f58546342e6386d4bac9a0641d6ed836318f6ea
世朋 梁 <lsp2323539801@outlook.com>
2023-11-16 00:42:56 +0800

    添加项目文件。

---
+++ number_distinct/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 2;
+//	for (i = 1; i <101; i++)
+//	{
+//		do {
+//			if (0 == i % j)
+//			{
+//				printf("%d为素数\n", i);
+//				j++;
+//				break;
+//			}
+//			else {
+//				printf("%d为质数\n", i);
+//				j++;
+//				break;
+//			}		
+//		} while (j < i);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int i = 0;
+//	int a = 0;
+//	int sum = 1;
+//	int total = 0;
+//	scanf("%d", &a);
+//	for (i = 1; i <= a; i++)
+//	{
+//		sum = sum * i;
+//		total += sum;
+//	}
+//	printf("%d\n", total);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
